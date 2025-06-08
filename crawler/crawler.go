6217bf04d689b5576e5e3914d15b61f0bd80f3e5
andriawan24 <fawaznaufal23@gmail.com>
2025-06-08 22:19:28 +0700

    feature: Add URL crawler

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,1 +1,45 @@
 package crawler
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"sprm-crawler/repository"
+	"strconv"
+
+	"github.com/gocolly/colly/v2"
+)
+
+func StartCrawlingUrl() {
+	c := colly.NewCollector()
+
+	c.OnHTML("li.last a", func(h *colly.HTMLElement) {
+		page, err := strconv.Atoi(h.Attr("data-page"))
+		if err != nil {
+			fmt.Println("Cannot parse last page", err)
+		}
+		page += 1
+
+		var frontiers []repository.UrlFrontier
+
+		for i := range page {
+			url := fmt.Sprintf("https://www.sprm.gov.my/index.php?id=21&page_id=96&page=%d&per-page=8", i+1)
+			id := sha256.Sum256([]byte(url))
+			frontiers = append(frontiers, repository.UrlFrontier{
+				ID:  hex.EncodeToString(id[:]),
+				Url: url,
+			})
+			fmt.Println(url)
+		}
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnScraped(func(r *colly.Response) {
+		fmt.Println("Successfully scraping", r.Request.URL)
+	})
+
+	c.Visit("https://www.sprm.gov.my/index.php?id=21&page_id=96&page=1&per-page=8")
+}
--- scraper/services/scraper_services.go
+++ scraper/services/scraper_services.go
@@ -1,1 +1,44 @@
 package services
+
+import (
+	"context"
+	"sprm-crawler/common"
+	"sprm-crawler/repository"
+
+	"github.com/rs/zerolog/log"
+	"github.com/samber/lo"
+)
+
+func UpsertExtraction(ctx context.Context, extractions []repository.Extraction) error {
+	context := context.Background()
+
+	tx, err := common.Pool.Begin(context)
+	if err != nil {
+		return err
+	}
+
+	queries := common.Query.WithTx(tx)
+
+	br := queries.UpsertExtraction(ctx, lo.Map(extractions, func(extraction repository.Extraction, _ int) repository.UpsertExtractionParams {
+		return repository.UpsertExtractionParams{
+			ID:            extraction.ID,
+			UrlFrontierID: extraction.UrlFrontierID,
+			SiteContent:   extraction.SiteContent,
+			ArtifactLink:  extraction.ArtifactLink,
+			RawPageLink:   extraction.RawPageLink,
+			Language:      extraction.Language,
+			PageHash:      extraction.PageHash,
+			Metadata:      extraction.Metadata,
+			CreatedAt:     extraction.CreatedAt,
+			UpdatedAt:     extraction.UpdatedAt,
+		}
+	}))
+
+	br.Exec(func(int, error) {
+		if err != nil {
+			log.Error().Err(err).Msg("Error upserting extractions")
+		}
+	})
+
+	return tx.Commit(ctx)
+}
